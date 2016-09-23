884d672a3f3297cb94cb1c84dc60e3471837089a
Clinton Lewis <clintonlewis17@gmail.com>
2016-09-22 21:03:00 -0400

    update simple regression

--- Clinton/simple_linear_regression.py
+++ Clinton/simple_linear_regression.py
@@ -8,7 +8,7 @@ Created on Thu Sep 22 13:36:38 2016
 import pandas as pd
 import numpy as np
 
-def training_error(y,w_hat,x):
+def training_error(w_hat):
     tr_error =[0.00]
     for i in range(x.shape[0])  :
         tr_error += np.power((y[i] - np.dot(w_hat.T,x[i])), 2)
@@ -29,4 +29,4 @@ y = df_num[:,[df_num.shape[1]-1]]
 # w_hat=(xx^T)^-1.x^T.y
 w_hat = np.dot(np.dot(np.linalg.inv(np.dot(x_t,x)),x_t),y)
 
-tr_error = training_error(y,w_hat,x)
+tr_error = training_error(w_hat)
