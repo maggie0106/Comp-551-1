0d395981628a25ac65044989a23b85fdafc10ce3
Clinton Lewis <clintonlewis17@gmail.com>
2016-09-22 23:38:02 -0400

    meand and std dev

--- Clinton/linear_reg_grad_des.py
+++ Clinton/linear_reg_grad_des.py
@@ -8,10 +8,12 @@ Created on Thu Sep 22 19:37:21 2016
 import pandas as pd
 import numpy as np
 
+# cost function
 def cost_function(q):
     cost = np.multiply(2,(np.dot(np.dot(x_t,x),w_hat[:,[q]])-np.dot(x_t,y)))
     return cost
-    
+
+# read the file    
 df = pd.read_csv(r'Book1.csv', header=None)
 df_num = df.values
 
@@ -20,7 +22,16 @@ x_t = np.ones(df_num.shape[0])
 
 for p in range(df_num.shape[1]-1):
     x_t = np.vstack((x_t,df_num[:,p]))
-    
+
+# find mean and std dev for every column
+x_mean = [0.0]
+for r in range(df_num.shape[1]-1):
+    x_mean = np.mean(x_t[:,[r]])
+    x_std = np.std(x_t[:,[r]])
+    # replace each row in given column with normalized data
+    for l in range(df_num.shape[0]-1):
+        x_t[l,r] = (x_t[l,r]-x_mean)/x_std
+        
 x = x_t.T
 y = df_num[:,[df_num.shape[1]-1]]
 
